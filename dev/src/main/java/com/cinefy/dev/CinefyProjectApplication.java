305fca7fb3a6b2ea1d339b330070b58c1f683b93
Zanettsu <viniciuszanetti248@gmail.com>
2025-02-25 22:41:23 -0300

    first commit
    Criação do projeto spring-java

---
+++ dev/src/main/java/com/cinefy/dev/CinefyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cinefy.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinefyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinefyProjectApplication.class, args);
+	}
+
+}
---
+++ dev/src/test/java/com/cinefy/dev/CinefyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinefy.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinefyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
